d1276ac0ebd225f81573b1f9822ccc4244933b79
MarySerna <mary.serna.8903@gmail.com>
2020-08-11 23:29:13 -0500

    WIP

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -8,16 +8,17 @@ def minOperations(n):
     """
     Calculates the fewest number of operations needed to result in exactly n H characters.
     """
+
+    if n <= 1:
+        return 0
+
     i = 2
     m = 0
     r = n
-    while True:
+    while r > 1 and i <= n:
         if r % i == 0:
             r /= i
             m += i
         else:
             i += 1
-        if r == 1 or i == n:
-            break
-
     return m
